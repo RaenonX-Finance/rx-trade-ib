e13c61e06fa895a4150a02cba35b8574ecc04921
RaenonX <raenonx0710@gmail.com>
2022-02-16 23:48:09 -0600

    Updated open orders type definition
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- trade_ibkr/utils/socket/open_order.py
+++ trade_ibkr/utils/socket/open_order.py
@@ -16,19 +16,19 @@ class OpenOrderData(TypedDict):
     px: float
 
 
-OpenOrderDict: TypeAlias = dict[int, list[OpenOrderData]]
+OpenOrderDict: TypeAlias = dict[int, dict[int, OpenOrderData]]
 
 
-def _from_open_orders(open_orders: list["OpenOrder"]) -> list[OpenOrderData]:
-    return [
-        {
+def _from_open_orders(open_orders: list["OpenOrder"]) -> dict[int, OpenOrderData]:
+    return {
+        open_order.order_id: {
             "orderId": open_order.order_id,
             "identifier": get_contract_identifier(open_order.contract),
             "side": open_order.side,
             "quantity": float(open_order.quantity),
             "px": open_order.price,
         } for open_order in open_orders
-    ]
+    }
 
 
 def to_socket_message_open_order(open_order: "OpenOrderBook") -> str:
