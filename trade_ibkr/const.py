37b68350310d9975ef028388c2e683cea2b0455e
RaenonX <raenonx0710@gmail.com>
2022-03-04 20:01:08 -0600

    Added config file
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- trade_ibkr/const.py
+++ trade_ibkr/const.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi_socketio import SocketManager
 from rich.console import Console
+import yaml
 
 console = Console()
 console_error = Console(stderr=True, style="bold red")
@@ -8,8 +9,16 @@ console_error = Console(stderr=True, style="bold red")
 fast_api = FastAPI()
 fast_api_socket = SocketManager(app=fast_api)
 
-SR_PERIOD = 120
-SR_MULTIPLIER = 1.2
+# ------ Load config
 
-AMPL_COEFF_TP = 2.2
-AMPL_COEFF_SL = 1
+with open("config.yaml", "r") as config_file:
+    config = yaml.safe_load(config_file)
+    # Print current config
+    console.print("[cyan]--- Config content ---[/cyan]")
+    console.print(yaml.dump(config, default_flow_style=False))
+
+SR_PERIOD = config["support-resistance"]["period"]
+SR_MULTIPLIER = config["support-resistance"]["multiplier"]
+
+AMPL_COEFF_TP = config["risk-management"]["take-profit-ampl"]
+AMPL_COEFF_SL = config["risk-management"]["stop-loss-ampl"]
