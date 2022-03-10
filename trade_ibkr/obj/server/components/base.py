b8ee0c79ec226217149d7e7df2d72b91b45336a6
RaenonX <raenonx0710@gmail.com>
2022-03-09 19:34:35 -0600

    Added false-negative error code ignoring
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- trade_ibkr/obj/server/components/base.py
+++ trade_ibkr/obj/server/components/base.py
@@ -12,6 +12,7 @@ from trade_ibkr.utils import print_error, print_log
 
 _error_code_ignore: set[int] = {
     202,  # Order canceled
+    1102,  # Connection restored
     2104,  # Market data farm connected
     2106,  # Historical market data farm connected
     2109,  # Out of RTH order is still processed
