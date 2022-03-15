98332479ac401bdf6f4896795969ff6604a5361e
RaenonX <raenonx0710@gmail.com>
2022-03-15 00:53:44 -0500

    Added `print_warning`
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- trade_ibkr/utils/__init__.py
+++ trade_ibkr/utils/__init__.py
@@ -3,7 +3,7 @@ from .contract import (
     make_futures_contract, make_crypto_contract, make_contract_from_unique_identifier,
     get_contract_identifier, get_detailed_contract_identifier,
 )
-from .log import print_log, print_error
+from .log import print_log, print_warning, print_error
 from .order import (
     make_market_order, make_limit_order, make_stop_order, get_order_trigger_price, make_limit_bracket_order,
     update_order_price,
--- trade_ibkr/utils/log.py
+++ trade_ibkr/utils/log.py
@@ -7,5 +7,9 @@ def print_log(message: str):
     console.print(f"[green]{datetime.now().strftime('%H:%M:%S.%f')[:-3]}[/green]: {message}")
 
 
+def print_warning(message: str):
+    console.print(f"[yellow]{datetime.now().strftime('%H:%M:%S.%f')[:-3]}: {message}[/yellow]")
+
+
 def print_error(message: str):
     console_error.print(f"[red]{datetime.now().strftime('%H:%M:%S.%f')[:-3]}[/red]: {message}")
