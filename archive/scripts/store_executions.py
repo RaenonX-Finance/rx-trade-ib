6b90293c2ff86625f57207276d62f13e464d51bc
RaenonX <raenonx0710@gmail.com>
2022-03-24 17:46:42 -0500

    Added execution storing script
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

---
+++ archive/scripts/store_executions.py
@@ -0,0 +1,27 @@
+from trade_ibkr.app import run_ib_server
+from trade_ibkr.model import OnExecutionFetchedEvent
+
+
+def main():
+    def store_executions():
+        app = run_ib_server()
+
+        async def fetched(e: OnExecutionFetchedEvent):
+            for identifier, df in e.executions.execution_dataframes.items():
+                dest = f"execution-{identifier}.csv"
+                print(f"Saved to: {dest}")
+                df.to_csv(dest)
+
+            app.disconnect()
+
+        app.set_on_executions_fetched(
+            on_execution_fetched=fetched,
+            on_execution_fetched_params=app._execution_on_fetched_params
+        )
+        app.request_all_executions()
+
+    store_executions()
+
+
+if __name__ == '__main__':
+    main()
